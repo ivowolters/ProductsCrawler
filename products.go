65fcfc09cc84a8dd6e16e627f852e648217f0db7
ivo wolters <ivowo2@gmail.com>
2024-03-04 18:50:26 +0100

    Loading sitemap to struct

--- products.go
+++ products.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
 
 	"github.com/gocolly/colly"
@@ -20,11 +21,28 @@ var sitemapPaths = []string{
 	"sitemap1.xml",
 }
 
+type Sitemap struct {
+	Urls []Url `xml:"url"`
+}
+
+type Url struct {
+	Loc string `xml:"loc"`
+}
+
 func fetchProductsFromUrl(url string) {
 	c := colly.NewCollector()
 
 	c.OnResponse(func(r *colly.Response) {
 		fmt.Println("Page visited: ", r.Request.URL)
+
+		var sitemap Sitemap
+
+		err := xml.Unmarshal(r.Body, &sitemap)
+
+		if err != nil {
+			fmt.Println("Error: %v\n", err)
+			return
+		}
 	})
 
 	for _, sitemapPath := range sitemapPaths {
